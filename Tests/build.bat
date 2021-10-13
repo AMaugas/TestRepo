a55dd8075ea7c23a981d9b22e5b890f31d5c8a89
AMaugas <antoine.maugas@braindex.fr>
2021-10-13 17:37:25 +0200

    fix: yep 4

--- Tests/build.bat
+++ Tests/build.bat
@@ -1,11 +1,11 @@
 @echo off
-SET BDS=C:\Program Files (x86)\Embarcadero\Studio\21.0
-SET BDSINCLUDE=C:\Program Files (x86)\Embarcadero\Studio\21.0\include
-SET BDSCOMMONDIR=C:\Users\Public\Documents\Embarcadero\Studio\21.0
-SET FrameworkDir=C:\Windows\Microsoft.NET\Framework\v4.0.30319
+SET BDS=D:\Program Files (x86)\Embarcadero\Studio\21.0
+SET BDSINCLUDE=D:\Program Files (x86)\Embarcadero\Studio\21.0\include
+SET BDSCOMMONDIR=D:\Users\Public\Documents\Embarcadero\Studio\21.0
+SET FrameworkDir=D:\Windows\Microsoft.NET\Framework\v4.0.30319
 SET FrameworkVersion=v4.5
 SET FrameworkSDKDir=
-SET PATH=%FrameworkDir%;%FrameworkSDKDir%;C:\Program Files (x86)\Embarcadero\Studio\21.0\bin;C:\Program Files (x86)\Embarcadero\Studio\21.0\bin64;C:\Program Files (x86)\Embarcadero\Studio\21.0\cmake;C:\Users\Public\Documents\Embarcadero\InterBase\redist\InterBaseXE7\IDE_spoof;%PATH%
+SET PATH=%FrameworkDir%;%FrameworkSDKDir%;D:\Program Files (x86)\Embarcadero\Studio\21.0\bin;D:\Program Files (x86)\Embarcadero\Studio\21.0\bin64;D:\Program Files (x86)\Embarcadero\Studio\21.0\cmake;D:\Users\Public\Documents\Embarcadero\InterBase\redist\InterBaseXE7\IDE_spoof;%PATH%
 SET LANGDIR=FR
 SET PLATFORM=
 SET PlatformSDK=
